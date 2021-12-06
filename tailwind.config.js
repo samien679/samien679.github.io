16f0c0b01fbd9859b38fe9ae5ce232806dbd2d5a
Sami Enholm <sami.enholm@gmail.com>
2021-12-06 14:13:28 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+       
+       'display': ['Road Rage'],
+       
+      },
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.blueGray,
+      green: colors.emerald,
+      fuchsia: colors.fuchsia,
+      blue: colors.cyan,
+    },
+    extend: {
+      backgroundImage: {
+         'herobg': "url('../img/bg.jpg?width=1280&height=720')",
+        }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
